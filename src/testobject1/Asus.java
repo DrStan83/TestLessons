f65ae6931ec48620b3da9621aecd324d95985178
DrStan83 <drstan83@gmail.com>
2019-05-05 16:19:57 +0300

    Add method Company

--- src/testobject1/Asus.java
+++ src/testobject1/Asus.java
@@ -4,4 +4,7 @@ public class Asus extends Notebook {
     public Asus(int hdd, String name){
         super(hdd, name);
     }
+    public void company (){
+        System.out.println("ASUS Co.");
+    }
 }
