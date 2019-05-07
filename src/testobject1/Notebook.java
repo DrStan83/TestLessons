8d55574baa4032876422fb60df98692c4f8d3791
DrStan83 <drstan83@gmail.com>
2019-05-08 00:42:47 +0300

    Slepok nomer 1 Nout

--- src/testobject1/Notebook.java
+++ src/testobject1/Notebook.java
@@ -1,10 +1,45 @@
 package testobject1;
 
-public class Notebook extends Computer{
-    public Notebook (int hdd, String name){
-        super(hdd, name);
+public abstract class Notebook extends Computer{
+    protected int cdSSD;
+    protected double wersionUsb;
+    protected boolean wiFi;
+
+    public Notebook() {
     }
-    public Notebook (boolean cdRom){
-        super(cdRom);
+
+    public int getCdSSD(){
+        return cdSSD;
     }
+
+    public void setCdSSD(int gb){
+        this.cdSSD = gb;
+    }
+
+    public double getWersionUsb(){
+        return wersionUsb;
+    }
+
+    public void setWersionUsb(double werUsb){
+        if(werUsb == 2.0 || werUsb == 3.0){
+            this.wersionUsb = werUsb;
+        }else{
+            System.out.println("Wersion USB not Supported");
+        }
+    }
+
+    public boolean getwiFi(){
+        return getwiFi();
+    }
+
+    public void setWiFi(boolean wifi){
+        if(wifi == true){
+            System.out.println("Wi-Fi connected");
+        }else {
+            System.out.println("Wi-Fi disconnected");
+        }
+    }
+
+        public abstract void batteryCharge();
+
 }
