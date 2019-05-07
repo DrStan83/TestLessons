21a1381f5a2cc0336bd71086732b00b51217b093
DrStan83 <drstan83@gmail.com>
2019-05-08 00:41:56 +0300

    Slepok nomer 1 Comp

--- src/testobject1/Computer.java
+++ src/testobject1/Computer.java
@@ -1,38 +1,15 @@
 package testobject1;
 
-public class Computer {
-
-    private String name;
-    private int ram;
-    private int hdd;
-    private double weight;
+abstract class Computer extends DeviceWhithOS {
 
     private String mBoard;
-    private String corpusMaterial;
     private boolean cdRom;
-    private boolean integratedVieo;
+    private boolean integratedVideo;
 
-    public Computer(int hdd, String name){
-        this.hdd = hdd;
-        this.name = name;
+    public Computer(){
+
     }
 
-    public Computer(String name){
-        this.name = name;
-  //      on();
-    }
-
-    public Computer (double weight){
-        this.weight = weight;
-    }
-
-    public Computer(int hdd){
-        this.hdd = hdd;
-    }
-
-    public Computer(boolean cdRom){
-        this.cdRom = cdRom;
-    }
 
     public Computer(String name, int ram, int hdd, double weight, String mBoard, String corpusMaterial, boolean cdRom, boolean integratedVieo) {
         this.name = name;
@@ -42,129 +19,57 @@ public class Computer {
         this.mBoard = mBoard;
         this.corpusMaterial = corpusMaterial;
         this.cdRom = cdRom;
-        this.integratedVieo = integratedVieo;
-      //  off();
+        this.integratedVideo = integratedVieo;
+
     }
 
-    public boolean getIntegratedVideo(){
-        return integratedVieo;
+    public boolean getIntegratedVideo() {
+        return integratedVideo;
     }
 
-    public void setIntegratedVieo(Boolean missOrNot){
-        if(integratedVieo == true) {
-            this.integratedVieo = missOrNot;
+    public void setIntegratedVieo(Boolean missOrNot) {
+        if (integratedVideo == true) {
+            this.integratedVideo = missOrNot;
             System.out.println("You may choose integratedVideo");
-        }else {
+        } else {
             System.out.println("In System miss integratedVideo");
         }
     }
 
-    public Boolean getCdRom(){
+    public Boolean getCdRom() {
         return cdRom;
     }
-    public void setCdRom(Boolean prezOrEps){
-        if(prezOrEps == true) {
+
+    public void setCdRom(Boolean prezOrEps) {
+        if (prezOrEps == true) {
             this.cdRom = prezOrEps;
             System.out.println("CdRom is Ready!!!");
-        }else {
+        } else {
             System.out.println("CdRom is missing");
         }
     }
 
-    public String getmBoard(){
+    public String getmBoard() {
         return mBoard;
     }
 
-    public void setmBoard(String stm){
-        if(stm.equals("Star")){
+    public void setmBoard(String stm) {
+        if (stm.equals("Star")) {
             System.out.println("Такой производитель не поддерживается");
-        }else{
+        } else {
             this.mBoard = stm;
         }
         System.out.println("Модель Материнской платы " + stm);
     }
 
-    public String getCorpusMaterial(){
-        return corpusMaterial;
-    }
-
-    public void setCorpusMaterial(String cM){
-        if(cM.equals("Бумага") || cM.equals("Полиетилен")){
-            System.out.println("Такое качество корпуса не приемлимо");
-        }else {
-            this.corpusMaterial = cM;
-        }
-        System.out.println("Corpus material " + cM);
-    }
+    public abstract void insertCd();
+    public abstract void readCD();
+    public abstract void blueScreen();
+    public abstract void reboot();
 
 
 
 
-    public String getName(){
-        return name;
-    }
-    public void setName(String newName){
-       this.name = newName;
-    }
-
-    public int getRam(){
-        return ram;
-    }
-    public void setRam(int newRam){
-        if(newRam > 0) {
-          this.ram = newRam;
-        }else{
-            System.out.println("Значение " + newRam + "Не может быть отрицательным");
-        }
-    }
-
-    public int getHdd(){
-        return hdd;
-    }
-    public void setHdd(int newHdd){
-        if(newHdd > 0) {
-            this.hdd = newHdd;
-        }else{
-            System.out.println("Значение " + newHdd + "Не может быть отрицательным");
-        }
-
-    }
-
-    public void insertCd(){
-       readCD("Чтение Диска");
-    }
-
-    private void readCD(String rcd){
-        setCdRom(true);
-        System.out.println(rcd);
-    }
-
-    public void blueScreen(){
-        reboot("Я Перезагружаюсь");
-    }
-
-    private void reboot(String reb){
-        off();
-        System.out.println(reb);
-        on();
-
-    }
 
 
-    private void print(String str){
-        System.out.println(str);
-    }
-
-
-    public void on(){
-        print(" I am On. My Model " + name);
-    }
-
-    public void off(){
-       print("I am Off");
-    }
-
-    public void load(){
-        print("I am Load. My HDD Space " + hdd + " Gb");
-    }
 }
