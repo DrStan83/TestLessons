bc7881597c523745c9977ff5145f590648b99b1a
DrStan83 <drstan83@gmail.com>
2019-05-17 17:55:43 +0300

    Test Class

--- src/testpackage/TestClass.java
+++ src/testpackage/TestClass.java
@@ -1,50 +1,31 @@
 package testpackage;
 
-import testobject1.Computer;
-import busobject.Bus;
-import testobject1.Notebook;
+import asus.AsusZbook;
+import toshiba.ToshibaSatelit;
 
 public class TestClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook(350, "ASUS");
-        notebook.on();
-        notebook.load();
-        notebook.off();
 
-/*        Computer comp = new Computer("IBM", 2048, 350,2, "Asus", "Plastic", true, true);
-        Computer comp1 = new Computer(350, "Intel");
-        Computer comp2 = new Computer("IBM");
+    AsusZbook zbook = new AsusZbook("Windows 10", "Asus Zbook", 2.8, "Aluminium",16384, 1024, 500, 3.0, true, "Asus x245", true, false, true, "Yellow");
+        zbook.on();
+        zbook.load();
+        zbook.activateFingerScan();
+        zbook.batteryCharge();
+        zbook.readCD();
+        zbook.changeColorKey();
+        zbook.blueScreen();
+        zbook.off();
+        zbook.ScanFinger(true);
 
-        comp = comp1;
-        comp1 = comp2;
-        comp2 = comp;
-
-
-        comp1.on();
-        comp2.on();
-
-
-        Bus bus1 = new Bus();
-        bus1.setOpenDoor(true);
-        bus1.stop();
-        bus1.loadPass();
-        bus1.setOpenDoor(false);
-        bus1.go();*/
-    //    bus1.go();
- //       Computer comp = new Computer();
-//        comp.setName("IBM");
-//        comp.setHdd(350);
-//        comp.setRam(2048);
-//        comp.on();
-//        comp.load();
-//        comp.off();
-//        comp.setmBoard("Asus");
-//        comp.setCorpusMaterial("Пластик");
-//        comp.setCdRom(true);
-//        comp.setIntegratedVieo(false);
-//        comp.insertCd();
-//        comp.blueScreen();
-
+        ToshibaSatelit satelit = new ToshibaSatelit("Windows 7", "Toshiba Satelit", 2.2, "Plastic", 8192, 500, 250, 2.0, true, "AsRock", true, true, 8,2.0);
+        satelit.on();
+        satelit.load();
+        satelit.playSound();
+        satelit.batteryCharge();
+        satelit.BlueToothOn();
+        satelit.readCD();
+        satelit.blueScreen();
+        satelit.off();
 
 
 
